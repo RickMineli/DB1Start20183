aee75d195ed3e432771cb15c543dd47a9b82bee9
Ricardo Mineli <rickmineli@hotmail.com>
2018-12-11 17:49:38 -0200

    {Aula 17} Divisao Controller,Service,Repository

---
+++ aula17/cidade-api/src/main/java/br/com/db1/cidade/cidadeapi/CidadeApiApplication.java
@@ -0,0 +1,12 @@
+package br.com.db1.cidade.cidadeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+public class CidadeApiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CidadeApiApplication.class, args);
+	}
+}
