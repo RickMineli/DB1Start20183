b4ae1f886745308f31837fe6a980e0b791272600
Ricardo Mineli <rick_mineli@hotmail.com>
2018-12-13 15:21:06 -0200

    {Aula 19} Criado GET, POST e classe DTO

--- aula18/calculadora-api/src/main/java/br/com/db1/db1start/aula18/calculadoraapi/controller/CalculadoraController.java
+++ aula18/calculadora-api/src/main/java/br/com/db1/db1start/aula18/calculadoraapi/controller/CalculadoraController.java
@@ -1,11 +1,16 @@
 package br.com.db1.db1start.aula18.calculadoraapi.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.db1.db1start.aula18.calculadoraapi.dto.CalculadoraDTO;
 import br.com.db1.db1start.aula18.calculadoraapi.model.Calculadora;
 import br.com.db1.db1start.aula18.calculadoraapi.model.Operacao;
 import br.com.db1.db1start.aula18.calculadoraapi.service.CalculadoraService;
@@ -13,13 +18,18 @@ import br.com.db1.db1start.aula18.calculadoraapi.service.CalculadoraService;
 @RestController
 @RequestMapping("/calculadora")
 public class CalculadoraController {
-	
+
 	@Autowired
 	private CalculadoraService calculadoraService;
 
+	@PostMapping
+	public Calculadora salvaOperacao(@RequestBody CalculadoraDTO calculadora) {
+		return calculadoraService.salvarOperacao(calculadora.getNumeroUm(), calculadora.getNumeroDois(),
+				calculadora.getOperacao());
+	}
+	
 	@GetMapping
-	public Calculadora salvaOperacao(@RequestParam("numeroUm") Double numeroUm,@RequestParam("numeroDois") Double numeroDois,@RequestParam("operacao") Operacao operacao) {
-		Calculadora calculadora = calculadoraService.salvarOperacao(numeroUm, numeroDois, operacao);
-		return calculadora;
+	public List<Calculadora> todasCalculadoras(){
+		return calculadoraService.buscarTodasCalculadoras();
 	}
 }
--- aula18/calculadora-api/src/main/java/br/com/db1/db1start/aula18/calculadoraapi/service/CalculadoraService.java
+++ aula18/calculadora-api/src/main/java/br/com/db1/db1start/aula18/calculadoraapi/service/CalculadoraService.java
@@ -1,6 +1,7 @@
 package br.com.db1.db1start.aula18.calculadoraapi.service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,13 +15,17 @@ public class CalculadoraService {
 	@Autowired
 	private CalculadoraRepository calculadoraRepository;
 
-	public Calculadora salvarOperacao(Double numeroUm,Double numeroDois, Operacao operacao) {
-		Calculadora calculadora = new Calculadora(numeroUm,numeroDois);
+	public Calculadora salvarOperacao(Double numeroUm, Double numeroDois, Operacao operacao) {
+		Calculadora calculadora = new Calculadora(numeroUm, numeroDois);
 		calculadora.setOperacao(operacao);
 		calculadora.setResultado(calculadora.executaOperacao(operacao));
 		calculadora.setDataOperacao(LocalDateTime.now());
 		calculadoraRepository.save(calculadora);
 		return calculadora;
+	}
+	
+	public List<Calculadora> buscarTodasCalculadoras (){
+		return calculadoraRepository.findAll();
 	}
 
 }
